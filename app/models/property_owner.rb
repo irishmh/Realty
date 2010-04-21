c99f604028197a54e1a88f1342abaacb6dc10c2e
unknown <owner@.(none)>
2010-04-20 21:29:19 -0500

    Added scaffold style sheets + got many show & index page table xrefs working

--- app/models/property_owner.rb
+++ app/models/property_owner.rb
@@ -1,6 +1,6 @@
 class PropertyOwner < ActiveRecord::Base
   # has_and_belongs_to_many:properties
   # has_and_belongs_to_many:demos
-  belongs_to :properties
-  belongs_to :demos
+  belongs_to :property
+  belongs_to :demo
 end
--- app/models/property_status.rb
+++ app/models/property_status.rb
@@ -1,5 +1,5 @@
 class PropertyStatus < ActiveRecord::Base
   # has_and_belongs_to_many:statuses
-  belongs_to :properties
-  belongs_to :statuses
+  belongs_to :property
+  belongs_to :status
 end
