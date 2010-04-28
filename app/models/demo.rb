5e213b2f04293e2b9948cb319dcadc6dd4a5e08d
unknown <owner@.(none)>
2010-04-27 22:21:18 -0500

    fixed state_cd in demo display/edit as no belongs_to

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,5 +1,6 @@
 class Demo < ActiveRecord::Base
   # has_one:role
   belongs_to :role
+  belongs_to :state
 
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,9 @@ class Property < ActiveRecord::Base
   # has_one:state
   belongs_to :state
   has_many:property_details
+  has_many:property_prices
+  has_many:property_owners
+  has_many:property_statuses
   
   def self.find_properties_for_sale
     find(:all, :order => "addr" )
